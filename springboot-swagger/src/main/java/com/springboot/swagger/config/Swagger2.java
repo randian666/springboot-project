d2b4ddadc57964a0ad5c5c86e1087295ff61cd5a
liuxun <liuxun@jd.com>
2017-03-21 14:13:56 +0800

    支持swagger、支持外部容器部署

---
+++ springboot-swagger/src/main/java/com/springboot/swagger/config/Swagger2.java
@@ -0,0 +1,38 @@
+package com.springboot.swagger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger2 RESTful已经json接口管理
+ * Created by liuxun on 2017/3/21.
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.springboot.swagger.action"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("接口管理平台")
+                .termsOfServiceUrl("http://www.jd.com/")
+                .contact("liuxun")
+                .version("1.0")
+                .build();
+    }
+}
---
+++ springboot-swagger/src/main/java/com/springboot/swagger/domain/User.java
@@ -0,0 +1,43 @@
+package com.springboot.swagger.domain;
+
+/**
+ * Created by liuxun on 2017/3/21.
+ */
+
+/**
+ *
+ * @author 程序猿DD
+ * @version 1.0.0
+ * @blog http://blog.didispace.com
+ *
+ */
+public class User {
+
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
