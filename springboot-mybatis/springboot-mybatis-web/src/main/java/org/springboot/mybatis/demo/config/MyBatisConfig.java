e5da24010c2f9fd6e9fb915d4e9586b64a1fce6a
liuxun <553798508@qq.com>
2017-04-04 18:54:07 +0800

    集成druid，并且实现监控。

--- springboot-mybatis/springboot-mybatis-web/src/main/java/org/springboot/mybatis/demo/config/MyBatisConfig.java
+++ springboot-mybatis/springboot-mybatis-web/src/main/java/org/springboot/mybatis/demo/config/MyBatisConfig.java
@@ -1,8 +1,31 @@
 package org.springboot.mybatis.demo.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.github.pagehelper.PageHelper;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContextException;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Properties;
 
 /**
  * 配置类
@@ -11,6 +34,87 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("org.springboot.mybatis.mapper")// mapper 接口类扫描包配置
 @ComponentScan(value = "org.springboot")//注解扫描
-public class MyBatisConfig {
+@EnableTransactionManagement
+public class MyBatisConfig implements EnvironmentAware {
+    private Environment environment;
+    private RelaxedPropertyResolver propertyResolver;
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+        this.propertyResolver = new RelaxedPropertyResolver(environment,"spring.datasource.");//配置前缀
+    }
+    //注册dataSource
+    @Bean(initMethod = "init", destroyMethod = "close")
+    public DruidDataSource dataSource() throws SQLException {
+        if (StringUtils.isEmpty(propertyResolver.getProperty("url"))) {
+            System.out.println("Your database connection pool configuration is incorrect!"
+                    + " Please check your Spring profile, current profiles are:"
+                    + Arrays.toString(environment.getActiveProfiles()));
+            throw new ApplicationContextException(
+                    "Database connection pool is not configured correctly");
+        }
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(propertyResolver.getProperty("driver-class-name"));
+        druidDataSource.setUrl(propertyResolver.getProperty("url"));
+        druidDataSource.setUsername(propertyResolver.getProperty("username"));
+        druidDataSource.setPassword(propertyResolver.getProperty("password"));
+        druidDataSource.setInitialSize(Integer.parseInt(propertyResolver.getProperty("initialSize")));
+        druidDataSource.setMinIdle(Integer.parseInt(propertyResolver.getProperty("minIdle")));
+        druidDataSource.setMaxActive(Integer.parseInt(propertyResolver.getProperty("maxActive")));
+        druidDataSource.setMaxWait(Integer.parseInt(propertyResolver.getProperty("maxWait")));
+        druidDataSource.setTimeBetweenEvictionRunsMillis(Long.parseLong(propertyResolver.getProperty("timeBetweenEvictionRunsMillis")));
+        druidDataSource.setMinEvictableIdleTimeMillis(Long.parseLong(propertyResolver.getProperty("minEvictableIdleTimeMillis")));
+        druidDataSource.setValidationQuery(propertyResolver.getProperty("validationQuery"));
+        druidDataSource.setTestWhileIdle(Boolean.parseBoolean(propertyResolver.getProperty("testWhileIdle")));
+        druidDataSource.setTestOnBorrow(Boolean.parseBoolean(propertyResolver.getProperty("testOnBorrow")));
+        druidDataSource.setTestOnReturn(Boolean.parseBoolean(propertyResolver.getProperty("testOnReturn")));
+        druidDataSource.setPoolPreparedStatements(Boolean.parseBoolean(propertyResolver.getProperty("poolPreparedStatements")));
+        druidDataSource.setMaxPoolPreparedStatementPerConnectionSize(Integer.parseInt(propertyResolver.getProperty("maxPoolPreparedStatementPerConnectionSize")));
+        druidDataSource.setFilters(propertyResolver.getProperty("filters"));
+        return druidDataSource;
+    }
 
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+        sqlSessionFactoryBean.setTypeAliasesPackage(propertyResolver.getProperty("typeAliasesPackage"));
+        //mybatis分页
+        PageHelper pageHelper = new PageHelper();
+        Properties props = new Properties();
+        props.setProperty("dialect", "mysql");
+        props.setProperty("reasonable", "true");
+        props.setProperty("supportMethodsArguments", "true");
+        props.setProperty("returnPageInfo", "check");
+        props.setProperty("params", "count=countSql");
+        pageHelper.setProperties(props); //添加插件
+        sqlSessionFactoryBean.setPlugins(new Interceptor[]{pageHelper});
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources(propertyResolver.getProperty("mapper-locations")));
+        return sqlSessionFactoryBean.getObject();
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager() throws SQLException {
+        return new DataSourceTransactionManager(dataSource());
+    }
+//    druid 开启监控
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings("/druid/*");
+        //reg.addInitParameter("allow", "127.0.0.1"); //白名单
+        //reg.addInitParameter("deny",""); //黑名单
+        reg.addInitParameter("loginUsername", "admin");
+        reg.addInitParameter("loginPassword", "admin");
+        return reg;
+    }
+
+    @Bean public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
 }
