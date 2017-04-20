58b238dd9ba22e61d5099758143c28240c5f37d2
liuxun <liuxun@jd.com>
2017-04-20 12:11:52 +0800

    sprinboot集成springcloud微服务。消费者端通过配置文件读取主题、discoveryClient方式消费

--- springboot-cloud/springboot-eureka-feign/src/main/java/com/springboot/cloud/eureka/feign/action/ConsumerController.java
+++ springboot-cloud/springboot-eureka-feign/src/main/java/com/springboot/cloud/eureka/feign/action/ConsumerController.java
@@ -1,11 +1,18 @@
 package com.springboot.cloud.eureka.feign.action;
 
+import com.netflix.appinfo.InstanceInfo;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import com.springboot.cloud.eureka.feign.service.ConsumerClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.List;
 
 /**
  *
@@ -15,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConsumerController {
     @Autowired
     ConsumerClient computeClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/testAdd", method = RequestMethod.GET)
     public String testAdd(@RequestParam Integer a, @RequestParam Integer b) {
@@ -23,5 +32,17 @@ public class ConsumerController {
     @RequestMapping(value = "/testSubtract", method = RequestMethod.GET)
     public String testSubtract(@RequestParam Integer a, @RequestParam Integer b) {
         return computeClient.subtractClient(a,b);
+    }
+
+    @RequestMapping(value = "/testDiscovery", method = RequestMethod.GET)
+    public String testDiscovery() {
+        List<ServiceInstance> list = discoveryClient.getInstances("compute-service");
+        if (list != null && list.size() > 0 ) {
+            URI uri = list.get(0).getUri();
+            if (uri !=null ) {
+                 return (new RestTemplate()).getForObject(uri+"/subtract?a=1&b=1",String.class);
+            }
+        }
+        return "";
     }
 }
