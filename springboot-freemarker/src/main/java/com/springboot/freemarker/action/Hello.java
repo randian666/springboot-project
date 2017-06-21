1446969f21202e6f46299c4102acac0a1203207d
liuxun <liuxun@jd.com>
2017-06-21 17:53:41 +0800

    springboot freemarker集成

--- springboot-freemarker/src/main/java/com/springboot/freemarker/action/Hello.java
+++ springboot-freemarker/src/main/java/com/springboot/freemarker/action/Hello.java
@@ -1,8 +1,11 @@
 package com.springboot.freemarker.action;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
 
 /**
  * Created by LiuXun on 2017/3/18.
@@ -13,5 +16,12 @@ public class Hello {
     @ResponseBody
     public String hello(){
         return "hello boot";
+    }
+
+    @RequestMapping("/freemarker")
+    public String welcome(ModelMap model) {
+        model.put("time", new Date());
+        model.put("message", "hello freemarker");
+        return "hello";
     }
 }
