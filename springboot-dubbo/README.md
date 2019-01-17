1eb1bed3c0db1f8968a6ef27e13703d1bce9f571
553798608@qq.com <553798608@qq.com>
2019-01-17 16:19:24 +0800

    springboot+dubbo+zk 直连实现

--- springboot-dubbo/README.md
+++ springboot-dubbo/README.md
@@ -2,6 +2,8 @@ springboot2.0+dubbo+zk集成
 ----------------------------
 1、安装zookeeper
 https://blog.csdn.net/qi49125/article/details/60779877
+#启动zk
+sh bin/zkServer.sh start-foreground
 2、引入dubbo-starter
  <dependency>
     <groupId>com.alibaba.boot</groupId>
--- springboot-dubbo/springboot-dubbo-consume/src/main/java/com/springboot/dubbo/consume/action/Hello.java
+++ springboot-dubbo/springboot-dubbo-consume/src/main/java/com/springboot/dubbo/consume/action/Hello.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 直连采用如下注解：
+ * @Reference(application = "${dubbo.application.id}",url = "dubbo://127.0.0.1:20880",version = "1.0.0")
  * Created by LiuXun on 2017/3/18.
  */
 @RestController
